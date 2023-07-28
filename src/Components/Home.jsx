6e667b2d54fdda0144acc073bfb4b70bf3a528a4
Abdur-Roqeeb Ismail <101981141+AbdurRoqeeb@users.noreply.github.com>
2023-07-29 00:37:51 +0100

    Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/tabelet_on_table.jpg";
+import image from "../images/tablet_on_table.jpg";
 
 const imageAltText = "data analysis";
 
